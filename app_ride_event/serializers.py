3919a7fa84b476ef4fda2ef063175ea7661b858f
Dariiius <dariusweek.2018@gmail.com>
2025-03-17 09:07:50 +0800

    Removed validation in RideEvent serializer

--- app_ride_event/serializers.py
+++ app_ride_event/serializers.py
@@ -9,18 +9,6 @@ class RideEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = RideEvent
         fields = '__all__'
-        
-    def validate(self, data):
-        """
-        Extra validation
-        """
-        request = self.context.get('request')
-        
-        # Checks if the ride already has an existing event in a POST request
-        if request and request.method == 'POST':
-            if RideEvent.objects.filter(ride_id=data.get('ride')).exists():
-                raise serializers.ValidationError({'message': 'A ride event already exists for this ride.'})
-        return data
 
     def create(self, validated_data):
         """
